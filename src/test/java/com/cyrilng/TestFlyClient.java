98da1f1b421d7fd8215cdcafe11f245a86e4fa2b
Cyril Ng Lung Kit <nglkitc@yahoo.co.uk>
2023-11-20 22:11:55 +0800

    Configure fly port

---
+++ src/test/java/com/cyrilng/TestFlyClient.java
@@ -0,0 +1,29 @@
+package com.cyrilng;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestFlyClient {
+    private static final Logger logger = LoggerFactory.getLogger(TestFlyClient.class);
+
+    public static void main(String[] args) {
+        logger.info("Setting up channel");
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("vanguard.fly.dev", 443).usePlaintext().build();
+
+        logger.info("Creating grpc stub with " + managedChannel);
+        VanguardServiceGrpc.VanguardServiceBlockingStub blockingStub = VanguardServiceGrpc.newBlockingStub(managedChannel);
+
+        VanguardRequest request = VanguardRequest.newBuilder().setName("Tester").build();
+        logger.info("Sending request " + request);
+
+        VanguardReply response =blockingStub.send(request);
+        logger.info("Received response " + response);
+        assertEquals(
+                "Hello Tester",
+                response.getMessage());
+    }
+}
